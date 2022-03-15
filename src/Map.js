2e5d50f6986c738dd8ca05ad911bcc9ad993302b
sammy-jay <sammyjayden27@gmail.com>
2022-03-15 11:45:16 +0100

    3 files modified

--- src/Map.js
+++ src/Map.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { Map as LeafletMap, TileLayer } from "react-leaflet";
-import "./Map.css";
-import { showDataOnMap } from "./util";
+import React from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import './Map.css';
+import { showDataOnMap } from './util';
 
 function Map({ countries, casesType, center, zoom }) {
   return (
     <div className="map">
-      <LeafletMap center={center} zoom={zoom}>
+      <MapContainer center={center} zoom={zoom}>
         <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
         {showDataOnMap(countries, casesType)}
-      </LeafletMap>
+      </MapContainer>
     </div>
   );
 }
